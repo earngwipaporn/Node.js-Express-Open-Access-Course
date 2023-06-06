113993c066df915023264133883d2463d5caac82
earngwipaporn <50837268+earngwipaporn@users.noreply.github.com>
2023-06-06 16:29:44 +0700

    update index.html

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import chalk from 'chalk';
-import Debug from "debug";
+import debug from "debug";
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) => {
 
-    res.send("Hello Earng Jaa");
+    res.send("Hello Earng naka");
 })
 
-const debug = Debug("app.js");
+const Debug = debug("app.js");
 app.listen(port, ()=>{
-    debug("Listening on port" + chalk.green(" : " +port));
+    Debug("Listening on port" + chalk.green(" : " +port));
 })
